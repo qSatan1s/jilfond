65fbda4f6207a058d93634fd8697f754cbe1a45c
k.bashlykov <qSatan1s@yandex.ru>
2024-08-21 17:00:04 +0500

    init

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:"./",//打包路径
+  base:"./",
   resolve: {
     alias:{
-      '@': path.resolve(__dirname, './src')//设置别名
+      '@': path.resolve(__dirname, './src')
     }
   },
-  server: {
-    port:4000,//启动端口
-    open: true,
-    proxy: {
-      // 选项写法
-      '/api': 'http://123.56.85.24:5000'//代理网址
-    },
-    cors:true
-  }
+
 
 })
